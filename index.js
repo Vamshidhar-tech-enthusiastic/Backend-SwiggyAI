466ce09dfbca3c7da7ac97827f2cd64a21b4cf82
Vamshidhar Reddy <thokala418@gmail.com>
2025-01-17 20:29:31 +0530

    updated server

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
-//import all required packages.In our case express(to run),mongoose(data modeling),dotenv(security),body-parser(json text)
-
-const express=require('express')
-const mongoose=require('mongoose')
-const dotenv=require('dotenv')
-const bodyParser=require('body-parser')
-const cors=require('cors')
-const vendorRoutes=require('./routes/vendorRoutes')
-const firmRoutes=require('./routes/firmRoutes')
+// Import required packages
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const vendorRoutes = require('./routes/vendorRoutes');
+const firmRoutes = require('./routes/firmRoutes');
 const productRoutes = require('./routes/productRoutes');
-const app=express()
-const PORT=9000
 
-dotenv.config()
-//mongodb connection
+const app = express();
+const PORT = 9000;
+dotenv.config();
 
-mongoose.connect(process.env.MONGO_URI)
-.then(()=>{  console.log("MongoDB connected");})
-.catch((error)=>{ console.log(error);})
-app.use(bodyParser.json())
-app.use(cors())
-app.use('/vendor',vendorRoutes)
-app.use('/firm',firmRoutes)
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch((error) => console.error('MongoDB connection error:', error));
+
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use('uploads/', express.static(path.join(__dirname, 'uploads')));
+app.use('/vendor', vendorRoutes);
+app.use('/firm', firmRoutes);
 app.use('/product', productRoutes);
-app.use('/uploads',express.static('/uploads'))
-//run the app
-app.listen(PORT,()=>
-{
-    console.log("Iam Ready at ",PORT)
-})
 
-//default route
-app.use('/home',(req,res)=>
-{
-    res.send("<center><h2 style='color:red'>Welcome to Swiggy AI</h2><center>")
-})
+app.get('/home', (req, res) => {
+    res.send("<center><h2 style='color:red'>Welcome to Swiggy AI</h2></center>");
+});
+
+app.options('*', cors());
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
