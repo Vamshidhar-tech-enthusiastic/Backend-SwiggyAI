ce39c6b1914ceed697948d73f960e314caa5b54a
Vamshidhar Reddy <thokala418@gmail.com>
2025-01-19 12:15:52 +0530

    updated cross origin requests

--- routes/firmRoutes.js
+++ routes/firmRoutes.js
@@ -7,10 +7,15 @@ router.post('/addFirm',verifyToken,firmController.addFirm)
 router.get('/getFirm',verifyToken,firmController.getFirm)
 router.delete('/:id',firmController.deleteFirmById)
 
-router.get('/uploads/:imageName',(req,res)=>
-{
-    const imgname=req.params.imageName;
-    res.headersSent('Content-typ','image/jpeg')
-    res.sendFile(path.join(__dirname,'..','uploads',imgname));
+router.get('/uploads/:imageName', (req, res) => {
+    const imgname = req.params.imageName;
+    const contentType = mime.lookup(imgname) || 'application/octet-stream'; 
+    res.setHeader('Content-Type', contentType); 
+    res.sendFile(path.join(__dirname, '..', 'uploads', imgname), (err) => {
+        if (err) {
+            
+            res.status(404).send('Image not found');
+        }
+    });
 });
 module.exports=router
