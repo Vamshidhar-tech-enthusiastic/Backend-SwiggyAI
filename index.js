a32d5433241572e546df86358c7258e2ffc0d102
Vamshidhar Reddy <thokala418@gmail.com>
2025-01-19 12:41:42 +0530

    updated cross origin requests

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PORT = 9000;
 dotenv.config();
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
     .catch((error) => console.error('MongoDB connection error:', error));
 
