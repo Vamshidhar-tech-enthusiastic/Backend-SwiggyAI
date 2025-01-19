19a05a3f89d973a8c4db7e49fd79358ff8c1b53f
Vamshidhar Reddy <thokala418@gmail.com>
2025-01-19 12:27:15 +0530

    updated cross origin requests

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 
 app.use(cors({
-        origin: 'http://localhost:5173',  
+        origin: '*',  
         methods: ['GET', 'POST', 'PUT', 'DELETE'], 
         allowedHeaders: ['Content-Type', 'Authorization'], 
         credentials: true,
