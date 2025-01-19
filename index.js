7af444d315ba9b399f62b5c24c8ad9f3b5a58a3a
Vamshidhar Reddy <thokala418@gmail.com>
2025-01-19 12:40:26 +0530

    updated cross origin requests

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 
     app.use(cors({
-        origin: 'https://backend-viggyai.onrender.com',  // Replace with your actual frontend domain
+        origin: '*',  // Replace with your actual frontend domain
         methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allowed methods
-        allowedHeaders: ['Content-Type', 'Authorization'],  // Allowed headers
+        // Allowed headers
     }));
     
 app.use(bodyParser.json());
